029fe2945fa0f1aab0fc304ef009ff09e0a46388
alhanayshaak <alhanaysha.ak@gmail.com>
2026-02-04 21:01:45 +0400

    fix: remove unused getConnectionStatus import from DeviceTile

--- src/components/DeviceTile.tsx
+++ src/components/DeviceTile.tsx
@@ -11,7 +11,7 @@ import {
   AlertCircle,
 } from 'lucide-react';
 import { Device } from '@/types/database';
-import { getConnectionStatus, formatBatteryLevel, formatLastSync } from '@/utils/devices';
+import { formatBatteryLevel, formatLastSync } from '@/utils/devices';
 
 interface DeviceTileProps {
   device: Device;
